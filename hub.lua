23cc4206950e1181305d848dc067a55b947e20df
chead-z <155196242+chead-z@users.noreply.github.com>
2024-02-15 06:21:17 -0300

    hub.lua

--- hub.lua
+++ hub.lua
@@ -258,7 +258,7 @@ local Section = Tab:AddSection({
 Tab:AddButton({
 	Name = "Shizukii Hub V2",
 	Callback = function(exec)
-      		print(loadstring(game:HttpGet('https://raw.githubusercontent.com/chead-z/lol/main/Hub2.0'))())
+      		print(loadstring(game:HttpGet("https://raw.githubusercontent.com/chead-z/lol/main/HubV2.lua"))();)
   	end    
 })
 
